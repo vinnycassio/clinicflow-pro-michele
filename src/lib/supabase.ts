9f1ca3f292a4a39c1495049bd549e9e498090e49
vinnycassio <42821015+vinnycassio@users.noreply.github.com>
2026-01-20 13:23:36 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -27,8 +27,8 @@ export const isSupabaseConfigured = (): boolean => {
   return Boolean(
     supabaseUrl && 
     supabaseAnonKey && 
-    supabaseUrl !== 'https://urqxtqfszqwbixpeeusd.supabase.co' &&
-    supabaseAnonKey !== 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVycXh0cWZzenF3Yml4cGVldXNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4ODU4OTAsImV4cCI6MjA4MzQ2MTg5MH0.TPHGDuG1gm7CJiaw7LdkRrVncX60nSZcDUEzWl0L850'
+    supabaseUrl !== 'https://wboumcppekffsqzyqmqd.supabase.co' &&
+    supabaseAnonKey !== 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indib3VtY3BwZWtmZnNxenlxbXFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg4NTY4OTIsImV4cCI6MjA4NDQzMjg5Mn0.erMs3Cy0RE4aLNOXFhrEow4GOEDgZcZnhkbL3Cdb2a8'
   );
 };
 
